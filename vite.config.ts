de9e20e27a95fd19ecc39b8f4754515507ca15db
GitanElyon <gitanmandellbalogh@gmail.com>
2025-09-30 13:43:14 -0400

    updated files for dev env

---
+++ vite.config.ts
@@ -0,0 +1,21 @@
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  plugins: [],
+  build: {
+    lib: {
+      entry: 'src/index.ts',
+      name: 'papyr',
+      fileName: (format) => `papyr.${format}.js`,
+    },
+    rollupOptions: {
+      external: ['codemirror', 'codemirror/lang-markdown'],
+      output: {
+        globals: {
+          codemirror: 'CodeMirror',
+          'codemirror/lang-markdown': 'CodeMirrorLangMarkdown',
+        },
+      },
+    },
+  },
+})
